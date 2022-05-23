605cbb9deaaf79877d1a58bd4a8e0d83c8458381
Samuel Muniz <d2021005503@unifei.edu.br>
2022-05-23 12:47:49 -0300

    Home

--- codigo.js
+++ codigo.js
@@ -19,3 +19,28 @@ function calcular() {
         document.getElementById('resultado').innerHTML = "nada..."
     }
 }
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
+    }
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("hkgfk"));
+    }
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+
+);
+mobileNavbar.init();
